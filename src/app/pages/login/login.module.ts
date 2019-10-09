a3c1e356718eb32b63bf7383af994e62a845e70c
Apple <siddharth@nisostech.com>
2019-10-09 20:22:46 +0530

    add ui validation

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
@@ -12,7 +13,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FormsModule
   ],
   exports: [
     RouterModule
